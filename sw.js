9273aac9544a82ecafddb75f9f27acc372daf2d4
circlhill-debug <circlhill@gmail.com>
2025-09-21 14:21:49 -0400

    Focus Mode replaces Collapse All; reinstate Monthly Glance by default; Quick Filter sparkline taller with padding; SW cache bump

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Route Stats PWA Service Worker (safe)
-const CACHE_VERSION = 'rs-pwa-v2025-09-20-06';
+const CACHE_VERSION = 'rs-pwa-v2025-09-20-07';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 
 const SAME_ORIGIN = self.location.origin;
